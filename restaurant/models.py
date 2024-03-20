cf7c9f9e3d7e2f02e1394293889b0ffb51236ba0
cia23 <carolynallen125@gmail.com>
2024-03-19 20:51:34 -0700

    update models

---
+++ restaurant/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Booking(models.Model):
+    # id = models.IntegerField()
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    reservation_date = models.DateField()
+
+
+class Menu(models.Model):
+   title = models.CharField(max_length=255) 
+   price = models.DecimalField(decimal_places=2, max_digits=10)
+   inventory = models.IntegerField()
