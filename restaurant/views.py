0cb967289a76f6124ff38c259833f6533dee197b
cia23 <carolynallen125@gmail.com>
2024-03-20 20:07:17 -0700

    add booking api endpoint

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 
 def index(request):
@@ -14,3 +15,8 @@ class MenuItemView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    # permission_classes = [permissions.IsAuthenticated]
